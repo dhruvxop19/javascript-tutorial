98fe492c1abb323691a27c8aecb887df11e33c8d
dhruvxop19 <88497117+dhruvxop19@users.noreply.github.com>
2025-06-21 18:26:39 +0530

    Add files via upload
    objects , string concatination , arrays , array of objects , objects of objects and usage of filters

---
+++ script.js
@@ -0,0 +1,16 @@
+// loop 1 to 5 
+//for ( let count = 1 ; count<= 5 ; count ++ ) {
+//console.log ("Dhruvieiei");
+//}
+//console.log ("loop has ended ")
+
+
+// calculate sum of 1 to 5 
+let sum = 0 
+for (
+    let i = 1 ; i <= 5 ; i++ ){
+        sum = sum + i ;
+    }
+    console.log ("sum = ", sum);
+    console.log ("loop has ended ");
+
